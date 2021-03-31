b40c04ce47f57e007d86b530b6e5268ae7a0fb21
Laura Miller <laura.miller03@bbc.co.uk>
2021-03-31 10:36:32 +0000

    Made pawns move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,11 +1,19 @@
 import Piece from './piece';
-
+import Square from '../square';
+import Player from '../../engine/player';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let square;
+        if (this.player == Player.BLACK) {
+            square = Square.at(currentSquare.row - 1, currentSquare.col);
+        } else {
+            square = Square.at(currentSquare.row + 1, currentSquare.col);
+        }
+        return [ square ];
+        // return [ Square.at(6, 7), Square.at(1, 0) ];
     }
 }
